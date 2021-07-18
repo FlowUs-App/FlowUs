a2c38beab3eb235393f7fd299f47e7564e43b830
lucas-goldner <lucas.goldner@googlemail.com>
2021-07-18 23:44:53 +0200

    Initial Commit

---
+++ FlowUs/FlowUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowUsApp.swift
+//  FlowUs
+//
+//  Created by Lucas Goldner on 18.07.21.
+//
+
+import SwiftUI
+
+@main
+struct FlowUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
