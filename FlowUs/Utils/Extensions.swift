54cea69f30f04a945f5aee1f6fc29773df352b96
lucas-goldner <lucas.goldner@googlemail.com>
2022-04-11 23:14:36 +0200

    feat: Finish glasmorphic card

--- FlowUs/Utils/Extensions.swift
+++ FlowUs/Utils/Extensions.swift
@@ -7,9 +7,37 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 extension UIScreen {
     static let screenWidth = UIScreen.main.bounds.size.width
     static let screenHeight = UIScreen.main.bounds.size.height
     static let screenSize = UIScreen.main.bounds.size
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let aVal, rVal, gVal, bVal: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (aVal, rVal, gVal, bVal) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (aVal, rVal, gVal, bVal) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (aVal, rVal, gVal, bVal) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (aVal, rVal, gVal, bVal) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(rVal) / 255,
+            green: Double(gVal) / 255,
+            blue:  Double(bVal) / 255,
+            opacity: Double(aVal) / 255
+        )
+    }
 }
--- FlowUs/Views/shared/display/GlasmorphicCard.swift
+++ FlowUs/Views/shared/display/GlasmorphicCard.swift
@@ -16,7 +16,16 @@ struct GlasmorphicCard: View {
 
             })
         }
-        .frame(width: UIScreen.screenWidth - 30, height: 250).mask(RoundedRectangle(cornerRadius: 15))
+        .frame(width: .infinity, height: 250).mask(RoundedRectangle(cornerRadius: 15, style: .continuous))
+        .overlay(RoundedRectangle(cornerRadius: 15, style: .continuous).stroke(lineWidth: 0.5).fill(Color.white).opacity(0.5))
+        .shadow(color: Color.black.opacity(0.2), x: 0, y: 20, blur: 20)
+        .padding()
+    }
+}
+
+struct LinearGradientPreview: View {
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.init(hexadecimal: "4624C2"), .init(hexadecimal: "7F5BFF")]), startPoint: .bottomTrailing, endPoint: .topLeading)
     }
 }
 
@@ -24,17 +33,17 @@ struct GlasmorphicVideo_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ZStack {
-                Color.purple
+                LinearGradientPreview()
                 GlasmorphicCard().previewDevice(PreviewDevice(rawValue: DeviceNames.iPhoneSE2.rawValue))
                     .previewDisplayName(DeviceNames.iPhoneSE2.rawValue)
             }
             ZStack {
-                Color.purple
+                LinearGradientPreview()
                 GlasmorphicCard().previewDevice(PreviewDevice(rawValue: DeviceNames.iPhone11.rawValue))
                     .previewDisplayName(DeviceNames.iPhone11.rawValue)
             }
             ZStack {
-                Color.purple
+                LinearGradientPreview()
                 GlasmorphicCard().previewDevice(PreviewDevice(rawValue: DeviceNames.iPhone13ProMax.rawValue))
                     .previewDisplayName(DeviceNames.iPhone13ProMax.rawValue)
             }
