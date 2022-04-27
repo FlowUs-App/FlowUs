da76ebb5465653767046b2ea5ff8660860f89842
lucas-goldner <lucas.goldner@googlemail.com>
2022-04-27 22:30:40 +0200

    fix: Verifyview darkblob overflow

--- FlowUs/Views/screen/VerifyView.swift
+++ FlowUs/Views/screen/VerifyView.swift
@@ -15,33 +15,38 @@ struct VerifyView: View {
     var body: some View {
         ZStack {
             CommonBackground()
-            DarkBlueBob()
-                .padding(.top, -UIScreen.screenWidth + 100)
             ScrollView {
-                BackBar()
-                    .frame(width: UIScreen.screenWidth)
-                    .padding(.top, -10)
-                HStack {
-                    HeadingB(text: "verify.header".l10n())
-                        .foregroundColor(.white)
-                    Icon(path: "ScreenAssets/Verify/YellowCone")
-                        .frame(width: 88, height: 96)
-                        .padding(.bottom, 20)
-                }.padding(.top, -28)
-                CommonText(text: "verify.description".l10n())
-                    .padding(.horizontal, 100)
-                    .foregroundColor(.white)
-                    .padding(.top, -32)
-                MagicNumersInput()
-                    .padding(.top, 52)
-                Icon(resize: true, path: "ScreenAssets/Verify/Saturn")
-                    .frame(width: 380, height: 380)
-                    .padding(.top, -60)
-                    .padding(.leading, 40)
-                    .rotationEffect(Angle(degrees: -15))
-                PrimaryButton(action: dummyFunction, text: "register.step.two".l10n())
-                    .frame(width: UIScreen.screenWidth)
-            }
+                ZStack {
+                    DarkBlueBob()
+                        .padding(.top, -UIScreen.screenWidth + 156)
+                        .frame(width: UIScreen.screenWidth)
+                    VStack {
+                        BackBar()
+                            .frame(width: UIScreen.screenWidth)
+                            
+                        HStack {
+                            HeadingB(text: "verify.header".l10n())
+                                .foregroundColor(.white)
+                            Icon(path: "ScreenAssets/Verify/YellowCone")
+                                .frame(width: 88, height: 96)
+                                .padding(.bottom, 20)
+                        }.padding(.top, -32)
+                        CommonText(text: "verify.description".l10n())
+                            .padding(.horizontal, 60)
+                            .foregroundColor(.white)
+                            .padding(.top, -32)
+                        MagicNumersInput()
+                            .padding(.top, 52)
+                        Icon(resize: true, path: "ScreenAssets/Verify/Saturn")
+                            .frame(width: 380, height: 380)
+                            .padding(.top, -72)
+                            .padding(.leading, 40)
+                            .rotationEffect(Angle(degrees: -15))
+                        PrimaryButton(action: dummyFunction, text: "register.step.two".l10n())
+                            .frame(width: UIScreen.screenWidth)
+                    }
+                }
+            }.frame(width: UIScreen.screenWidth)
         }
     }
 }
